b43c22526d51c43aa9e3d060fcb212cf10b9bd5f
teomusacchio <teomusacchio@google.com>
2023-09-17 08:56:53 +0200

    rasa pulito

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,36 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+from rasa_sdk.events import SlotSet
 
+class ActionCheckPersonName(Action):
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def name(self) -> Text:
+        return "action_check_person_name"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        person_name = tracker.get_slot("person_name")
+        print(f"---- DEBUGGING ----\nValore estratto per 'person_name': {person_name}")
+    
+
+        # Qui puoi definire una lista di nomi che il bot riconosce
+        known_names = ["Mario Rossi", "Giuseppe Verdi", "Anna Bianchi"]
+
+        person_name = tracker.get_slot("person_name")
+        
+        # Aggiungi le seguenti righe:
+        print("---- DEBUGGING ----")
+        print(f"Valore estratto per 'person_name': {person_name}")
+
+        if person_name in known_names:
+            response = f"Conosco {person_name}! È nel nostro database."
+        else:
+            response = f"Mi dispiace, ma non ho informazioni su {person_name}."            
+        # Aggiungi anche questo:
+        print(f"Risposta generata: {response}")
+    
+        dispatcher.utter_message(text=response)
+        return []
+
